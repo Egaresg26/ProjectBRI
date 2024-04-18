523166681f11b6490eb59a50190355b7e7711a82
Egaresg26 <ega26rg@gmail.com>
2024-04-18 15:00:23 +0700

    updates

--- ProjectJboss/autojboss.ps1
+++ ProjectJboss/autojboss.ps1
@@ -1,39 +1,69 @@
-# Jalankan file batch menggunakan Invoke-Item (ini akan membuka file batch dalam jendela CMD yang terpisah)
-#Invoke-Item -Path ".\LoginT24.py"
-#Start-Sleep -Seconds 5
-# Invoke-Item -Path ".\ping.bat"
-# Start-Sleep -Seconds 5
-# Jalankan file batch
-Start-Process -FilePath .\Run.bat -Verb RunAs
+#STOP TSA SERVICE
+# buka file trun.bat
+Add-Type @"
+    using System;
+    using System.Runtime.InteropServices;
+    public class WinApi {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SwitchToThisWindow(IntPtr hWnd, bool fAltTab);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+    }
+"@
+
+Start-Process -FilePath .\quit.bat -Verb RunAs
+Add-Type -AssemblyName System.Windows.Forms
+Start-Sleep -Seconds 3
+
+#LOGIN
+#username
+[System.Windows.Forms.SendKeys]::SendWait("username{ENTER}")
+Start-Sleep -Seconds 3
+#password
+[System.Windows.Forms.SendKeys]::SendWait("passwor{ENTER}")
+Start-Sleep -Seconds 3
+
+#masuk cabang SGP
+[System.Windows.Forms.SendKeys]::SendWait("SGP{ENTER}")
+Start-Sleep -Seconds 3
+
+#Mulai stop TSA service
+[System.Windows.Forms.SendKeys]::SendWait("TS, I TSM{ENTER}")
+Start-Sleep -Seconds 3
+[System.Windows.Forms.SendKeys]::SendWait("6{ENTER}")
+Start-Sleep -Seconds 3
+[System.Windows.Forms.SendKeys]::SendWait("STOP{ENTER}")
+Start-Sleep -Seconds 3
+[System.Windows.Forms.SendKeys]::SendWait("^V{ENTER}") 
+
+Start-Sleep -Seconds 3
+
+#kembali ke menu utama
+[System.Windows.Forms.SendKeys]::SendWait("^u{ENTER}")
+Start-Sleep -Seconds 3
+[System.Windows.Forms.SendKeys]::SendWait("^u{ENTER}")
+Start-Sleep -Seconds 3
 
 # Tunggu beberapa detik agar jendela CMD selesai dimuat
 Start-Sleep -Seconds 5
 
-# Temukan jendela CMD yang terbuka
-#$cmdWindow = Get-Process | Where-Object { $_.MainWindowTitle -eq "C:\Windows\system32\cmd.exe"}
+#STOP JBOSS
+#Get-Process | Where-Object { $_.MainWindowTitle -ne "" } | Select-Object MainWindowTitle
 
+$windowName = "Administrator: Command Prompt - ping  google.com -t"
+$windowProcess = Get-Process | Where-Object { $_.MainWindowTitle -eq $windowName }
+$windowHandle = $windowProcess.MainWindowHandle
+[WinApi]::SwitchToThisWindow($windowHandle, $true)
 
-Add-Type -AssemblyName System.Windows.Forms
-[System.Windows.Forms.SendKeys]::SendWait("2{ENTER}")
+# Kirimkan perintah ping
+#[System.Windows.Forms.SendKeys]::SendWait(" ping google.com -t{ENTER}")
 
-# Tunggu beberapa detik
-Start-Sleep -Seconds 5
+Start-Sleep -Seconds 10
 
-# Temukan jendela CMD lagi (diasumsikan sekarang menjadi "Administrator: Command Prompt")
-#$cmdWindow = Get-Process | Where-Object { $_.MainWindowTitle -eq "Administrator: Command Prompt" }
-
-# Kirimkan perintah untuk melakukan ping ke google.com secara terus-menerus
-
-Add-Type -AssemblyName PresentationFramework
-Add-Type -AssemblyName PresentationCore
-Add-Type -AssemblyName WindowsBase
-
-# Dapatkan jendela aktif
-#$activeWindow = [System.Windows.Application]::Current.Windows | Where-Object { $_.IsActive }
-$activeWindow.WindowState = [System.Windows.WindowState]::Minimized
-
-[System.Windows.Forms.SendKeys]::SendWait(" ping google.com -t{ENTER}")
-
-Start-Sleep -Seconds 5
-
+# Mengirimkan input Ctrl+C untuk menghentikan ping
 [System.Windows.Forms.SendKeys]::SendWait(" ^c")
--- ProjectJboss/cobaswitch.ps1
+++ ProjectJboss/cobaswitch.ps1
@@ -1,32 +1,112 @@
-# Cek apakah tipe WinApi sudah ada sebelumnya
-if (-not ([System.Management.Automation.PSTypeName]'WinApi').Type) {
-    # Tipe WinApi belum ada, maka tambahkan definisi tipe
-    Add-Type @"
-        using System;
-        using System.Runtime.InteropServices;
-        public class WinApi {
-            [DllImport("user32.dll")]
-            [return: MarshalAs(UnmanagedType.Bool)]
-            public static extern bool SwitchToThisWindow(IntPtr hWnd, bool fAltTab);
-        }
+# Load WinApi functions
+Add-Type @"
+    using System;
+    using System.Runtime.InteropServices;
+    public class WinApi {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SwitchToThisWindow(IntPtr hWnd, bool fAltTab);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+    }
 "@
+
+# Run quit.bat as administrator
+Start-Process -FilePath .\quit.bat -Verb RunAs
+Add-Type -AssemblyName System.Windows.Forms
+
+# Wait for quit.bat to complete
+Start-Sleep -Seconds 3
+
+# Send username
+[System.Windows.Forms.SendKeys]::SendWait("username{ENTER}")
+
+Start-Sleep -Seconds 3
+
+# Send password
+[System.Windows.Forms.SendKeys]::SendWait("password{ENTER}")
+Start-Sleep -Seconds 3
+
+# Enter SGP branch
+[System.Windows.Forms.SendKeys]::SendWait("SGP{ENTER}")
+Start-Sleep -Seconds 3
+
+# Stop TSA service
+[System.Windows.Forms.SendKeys]::SendWait("TS, I TSM{ENTER}")
+Start-Sleep -Seconds 3
+[System.Windows.Forms.SendKeys]::SendWait("6{ENTER}")
+Start-Sleep -Seconds 3
+[System.Windows.Forms.SendKeys]::SendWait("STOP{ENTER}")
+Start-Sleep -Seconds 3
+
+# Paste command from clipboard
+[System.Windows.Forms.SendKeys]::SendWait("^V{ENTER}")
+Start-Sleep -Seconds 3
+
+# Return to main menu
+[System.Windows.Forms.SendKeys]::SendWait("^U{ENTER}")
+Start-Sleep -Seconds 3
+[System.Windows.Forms.SendKeys]::SendWait("^U{ENTER}")
+Start-Sleep -Seconds 3
+
+# Wait for CMD window to finish loading
+Start-Sleep -Seconds 5
+
+# Stop JBOSS
+$windowName = "Administrator: Command Prompt - ping  google.com -t"
+$windowProcess = Get-Process | Where-Object { $_.MainWindowTitle -eq $windowName }
+
+# Periksa apakah proses jendela ditemukan
+if ($windowProcess) {
+    $windowHandle = $windowProcess.MainWindowHandle
+    if ($windowHandle -ne [IntPtr]::Zero) {
+        [WinApi]::SwitchToThisWindow($windowHandle, $true)
+    } else {
+        Write-Host "Handle jendela kosong atau tidak valid."
+    }
+} else {
+    Write-Host "Proses jendela tidak ditemukan."
 }
 
-# Cari proses dengan nama "powershell" dan judul jendela "Administrator: Windows PowerShell"
-$windowProcess = Get-Process | Where-Object { $_.ProcessName -eq "powershell" -and $_.MainWindowTitle -eq "Administrator: Windows PowerShell" }
+# Tunggu jendela command prompt untuk diaktifkan
+Start-Sleep -Seconds 10
 
-if ($windowProcess -ne $null) {
-    # Dapatkan handle jendela
-    $windowHandle = $windowProcess.MainWindowHandle
+# Kirimkan Ctrl+C untuk menghentikan ping
+[System.Windows.Forms.SendKeys]::SendWait("^C")
 
-    # Beralih ke jendela menggunakan fungsi Win32 API
-    [WinApi]::SwitchToThisWindow($windowHandle, $true)
-    # Tambahkan penundaan sejenak untuk memastikan beralih jendela selesai
-    Start-Sleep -Seconds 5
+Start-Sleep -Seconds 5
 
-    # Kirimkan perintah ping menggunakan SendKeys
-    Add-Type -AssemblyName System.Windows.Forms
-    [System.Windows.Forms.SendKeys]::SendWait("ping google.com -t")
+#[System.Windows.Forms.SendKeys]::SendWait("Y{ENTER}")
+#Start-Sleep -Seconds 5
+
+# Menutup jendela CMD yang sedang berjalan
+if ($windowProcess) {
+    $windowProcess.CloseMainWindow()
 } else {
-    Write-Host "Jendela 'Administrator: Windows PowerShell' tidak ditemukan."
+    Write-Host "Proses jendela tidak ditemukan untuk ditutup."
+}
+
+#Buka Jshell.bat 
+Start-Process -FilePath .\quit.bat -Verb RunAs
+Add-Type -AssemblyName System.Windows.Forms
+Start-Sleep -Seconds 3
+
+# buka DBTools
+[System.Windows.Forms.SendKeys]::SendWait("DBTools{ENTER}")
+Start-Sleep -Seconds 3
+
+[System.Windows.Forms.SendKeys]::SendWait("JQL CLEAR-FILE F.TSA.STATUS WITH @ID NOT LIKE ...OLTP... {ENTER}")
+Start-Sleep -Seconds 3
+
+[System.Windows.Forms.SendKeys]::SendWait("x{ENTER}")
+Start-Sleep -Seconds 3
+
+if ($windowProcess) {
+    $windowProcess.CloseMainWindow()
+} else {
+    Write-Host "Proses jendela tidak ditemukan untuk ditutup."
 }
